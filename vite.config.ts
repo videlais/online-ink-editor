dfc671f9b3b472c743e09921fbe2671b3eebf068
Dan Cox <dan.cox@videlais.com>
2026-04-03 17:37:40 -0500

    Updating deps and preparing for new, minor version update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react-dom') || id.includes('node_modules/react/')) {
+            return 'react';
+          }
+          if (id.includes('node_modules/@codemirror/') || id.includes('node_modules/@mavnn/codemirror-lang-ink') || id.includes('node_modules/@uiw/react-codemirror')) {
+            return 'codemirror';
+          }
+          if (id.includes('node_modules/inkjs')) {
+            return 'ink';
+          }
+        },
+      },
+    },
+  },
 });
